9c5ab413c79dc8b147351f40635d40f28c7eb238
JevinJ <jevinjones@openmailbox.org>
2017-08-14 19:51:58 -0700

    Tweaks and dataset changes
    -No longer using last 4 measurements of price in favor of most recent. Will test both versions for accuracy/errors in the future.
    
    -Cleaned up and simplified most variables names/functions/tests.

--- src/BittrexNotify.py
+++ src/BittrexNotify.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+import os
 import glob
 import time
 from decimal import *
@@ -10,43 +11,57 @@ http = urllib3.PoolManager()
 page = bs(http.request('GET', 'https://bittrex.com/api/v2.0/pub/Markets/GetMarketSummaries').data)
 data = json.loads(str(page))
 
-#todo: change this, ambiguous
-def print_prices(prices_list):
-	prices_list = sorted(prices_list, key=lambda t: t[1], reverse=True)
-	max_len = len(max(prices_list, key=lambda x:len(x[0]))[0])
-	for i in prices_list:
-		print(i[0] + (' ' * ((max_len+1)-len(i[0]))) + str(i[1]))
+
+def print_ticker(latest_data_list):
+	if not latest_data_list:
+		return
+	latest_data_list.sort(key=lambda x: x[1], reverse=True)
+	max_len = len(max(latest_data_list, key=lambda x:len(x[0]))[0])
+	for i in latest_data_list:
+		print(i[0] + (' ' * ((max_len+1)-len(i[0]))) + i[1])
 		
 def save_pickle(prices_dict):
 	date_time = time.strftime('%H%M', time.localtime())
-	with open('price_pickles/'+date_time+'.pickle', 'wb') as f:
+	with open('history/'+date_time+'.pickle', 'wb') as f:
 		pickle.dump(prices_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 		
 def open_pickle():
-	glob_files = glob.glob('price_pickles/*pickle')
-	for file_name in glob_files:
-		print(file_name)
-		with open(file_name, 'rb') as f:
-			yield pickle.load(f)
+	glob_files = glob.glob('history/*pickle')
+	if not glob_files:
+		return []
+	glob_files.sort(key=os.path.getmtime, reverse=True)
+	with open(glob_files[0], 'rb') as f:
+		print(glob_files[0])
+		return pickle.load(f)
+
 	
-prices_dict = {}
-prices_list = []
-for i in data['result']:
-	if i['Market']['BaseCurrency'] == 'BTC':
+while(True):
+	latest_data = {}
+	latest_data_list = []
+	for i in data['result']:
 		name = i['Market']['MarketCurrencyLong']
-		last = i['Summary']['Last']
-		prev_day = i['Summary']['PrevDay']
-		perc_change = (((last-prev_day)/prev_day)*100)
-		prices_dict[name] = float('{:.02f}'.format(perc_change))
-		prices_list.append((name, float('{:.02f}'.format(perc_change))))
-
-#print_prices(prices_list)
-
-#todo: compare file names to sort by latest
-last_dicts = list(open_pickle())
-save_pickle(prices_dict)
-
-
-
+		last_price = i['Summary']['Last']
+		yday_price = i['Summary']['PrevDay']
+		last_vol = i['Summary']['BaseVolume']
+		if i['Market']['BaseCurrency'] == 'BTC' and last_vol >= 350:
+			change = (((last_price-yday_price)/yday_price)*100)
+			latest_data[name] = i
+			latest_data_list.append([name, '{:.02f}'.format(change)])
+	#print_ticker(latest_data_list)
 	
 	
+	prev_data = dict(open_pickle())
+	to_notify = []
+	if prev_data:
+		for name in latest_data:
+			last_price = latest_data[name]['Summary']['Last']
+			prev_data_price = prev_data[name]['Summary']['Last']
+			if (last_price - prev_data_price) > 3:
+				print_ticker([name, '+'+'{:.02f}'.format(last_price - prev_data_price)])
+				to_notify.append((name, float('{:.02f}'.format(last_price))))
+				
+	
+	save_pickle(latest_data)
+	
+	print("SLEEPING")
+	time.sleep(350)
