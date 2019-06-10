0dc33c57e67f8600d0331ea0939cafce4c9677a2
JevinJ <jevinjones@openmailbox.org>
2019-06-09 21:50:38 -0700

    Setup new Application class

--- src/main.py
+++ src/main.py
@@ -1,26 +1,9 @@
-import tkinter as tk
-import config
-from GUIfasttick import GUIfasttick
-from GUIslowtick import GUIslowtick
+from PyQt5.QtWidgets import QApplication, QWidget
 
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-
-        # Images used by GUI
-        self.downArrow = tk.PhotoImage(file='media/down_arrow.png')
-        self.upArrow = tk.PhotoImage(file='media/up_arrow.png')
-        self.noArrow = tk.PhotoImage(file='media/no_arrow.png')
-        self.notifyBell = tk.PhotoImage(file='media/notification_bell.png')
-        self.questionMark = tk.PhotoImage(file='media/question_mark.png')
-
-        self.slowMarket = GUIslowtick(self)
-        self.fastMarket = GUIfasttick(self)
-
-        self.mainloop()
+class Application:
+    def __init__(self):
+        self.app = QApplication()
+        self.app.exec_()
 
 if __name__ == '__main__':
     app = Application()
-    app.master.title('BittrexNotify')
