82538b5fda0e3d5288bf1d906927d6d989fce4e4
JevinJ <jevinjones@openmailbox.org>
2019-06-10 23:29:48 -0700

    Add setup_menu_bar(), setup_tool_bar()

--- src/main.py
+++ src/main.py
@@ -3,18 +3,23 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QLabel, QComboBo
 class Application(QMainWindow):
     def __init__(self):
         super().__init__()
-        menu_bar = self.menuBar()
-        tool_bar = self.addToolBar('ToolBar')
-
         self.setWindowTitle('Coin Monitor')
         self.setGeometry(300, 300, 350, 200)
+        self.setup_menu_bar()
+        self.setup_tool_bar()
+        self.show()
 
+    def setup_menu_bar(self):
+        menu_bar = self.menuBar()
         exit_action = QAction('Exit', self)
         exit_action.setShortcut('Ctrl+Q')
         exit_action.triggered.connect(self.close)
 
         file_menu = menu_bar.addMenu('File')
         file_menu.addAction(exit_action)
+
+    def setup_tool_bar(self):
+        tool_bar = self.addToolBar('ToolBar')
 
         website_select_box = QComboBox()
         website_select_box.addItems(['Bittrex', 'Binance'])
@@ -23,8 +28,6 @@ class Application(QMainWindow):
 
         tool_bar.addWidget(website_select_label)
         tool_bar.addWidget(website_select_box)
-
-        self.show()
 
 
 if __name__ == '__main__':
