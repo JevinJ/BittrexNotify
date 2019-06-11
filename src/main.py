33d1e928dedf5bea7b0eefc46926597ef4fd6bde
JevinJ <jevinjones@openmailbox.org>
2019-06-10 23:23:40 -0700

    Add tool bar and place site selector ComboBox into tool bar.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QLabel, QComboBox, QHBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QLabel, QComboBox, QHBoxLayout, QGridLayout, QWidget, QToolBar
 
 class Application(QMainWindow):
     def __init__(self):
         super().__init__()
         menu_bar = self.menuBar()
-        status_bar = self.statusBar()
+        tool_bar = self.addToolBar('ToolBar')
 
         self.setWindowTitle('Coin Monitor')
         self.setGeometry(300, 300, 350, 200)
@@ -20,11 +20,10 @@ class Application(QMainWindow):
         website_select_box.addItems(['Bittrex', 'Binance'])
         website_select_label = QLabel("Website:")
         website_select_label.setBuddy(website_select_box)
-        status_bar.addWidget(website_select_label)
-        status_bar.addWidget(website_select_box, stretch=1)
+        tool_bar.addWidget(website_select_label)
+        tool_bar.addWidget(website_select_box)
 
         self.show()
-
 
 if __name__ == '__main__':
     import sys
