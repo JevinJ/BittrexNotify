db904768d307e4d16a5ba3e9ef537093b8030f62
JevinJ <jevinjones@openmailbox.org>
2019-06-10 23:51:04 -0700

    Add api_address property, get().

--- src/website/website.py
+++ src/website/website.py
@@ -1,3 +1,18 @@
+import requests
+
 class Website:
-    def __init__(self):
-        self.api_address = ''
+    def __init__(self, api_address):
+        self._api_address = api_address
+
+    def get(self):
+        response = requests.get(self.api_address)
+        if response.status_code != 200:
+            raise ConnectionError(f'Failed to connect with api, status code: {response.status_code}')
+        return response
+
+    @property
+    def api_address(self):
+        return self._api_address
+
+
+
