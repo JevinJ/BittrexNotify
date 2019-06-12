61ce5c6e748a7cfe7d43248a817266c2f96a1b14
JevinJ <jevinjones@openmailbox.org>
2019-06-11 22:49:39 -0700

    Implement yaml config urls for Website class, remove api_address property.

--- src/website/website.py
+++ src/website/website.py
@@ -1,19 +1,22 @@
 import requests
+import yaml
 
 class Website:
-    def __init__(self, api_address):
-        self._api_address = api_address
+    def __init__(self, website_name):
+        website_name = website_name.lower()
+        with open('websites.yaml') as website_configs:
+            site_config = yaml.load(website_configs)[website_name]
+            self._GET_markets_url = site_config['GET_markets_url']
+            self._GET_markets_summary_url = site_config['GET_markets_summary_url']
+            self._GET_market_symbol_url = site_config['GET_market_symbol_url']
+            self._GET_market_symbol_summary_url = site_config['GET_market_symbol_summary_url']
 
     @property
-    def api_address(self) -> str:
-        return self._api_address
-
-    @property
-    def summary(self) -> dict:
+    def summary(self) -> list:
         return self._get_summary()
 
     def _get_summary(self):
-        response = requests.get(self.api_address)
+        response = requests.get(self._GET_markets_summary_url)
         response.raise_for_status()
         return response.json()
 
