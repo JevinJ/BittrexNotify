178e910c21dd877fdb86ac4e6c0c24db18ce7b74
JevinJ <jevinjones@openmailbox.org>
2019-06-11 22:50:18 -0700

    Change super() call for new Website class signature.

--- src/website/bittrex.py
+++ src/website/bittrex.py
@@ -2,5 +2,5 @@ from .website import Website
 
 class Bittrex(Website):
     def __init__(self):
-        super().__init__(api_address='https://api.bittrex.com/v3/markets/summaries')
+        super().__init__(website_name='bittrex')
 
